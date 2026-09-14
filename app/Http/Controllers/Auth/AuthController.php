9982589913d097e4a319a377ce506972693a1e55
Tarun Birla <tarunbirla2018@gmail.com>
2026-09-14 14:11:31 +0530

    tarry

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Models\Group;
 use App\Mail\PasswordResetMail;
+use App\Services\StripePaymentService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
